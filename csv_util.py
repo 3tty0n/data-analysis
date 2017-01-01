c808255e3fe94dfcdfa9e54cf581598615119f36
Yusuke Izawa <yuizalp@gmail.com>
2017-01-02 01:26:27 +0900

    fix csv_util to keep city name

--- csv_util.py
+++ csv_util.py
@@ -1,24 +1,23 @@
-import csv
 import numpy as np
 import pandas as pd
+from columns import columns
 
 
 def to_log_pandas():
-    df = pd.read_csv('data/csv/input.csv', header=None, skiprows=1, encoding='utf-8')
-    df = df.drop(12, axis=1)
+    df = pd.read_csv('data/csv/input.csv')
+    df_name = df['name']
+    df_dropped = df.drop('name', axis=1)
 
-    df_arr = np.array(df).T
+    df_arr = np.array(df_dropped).T
     res_arr = []
     for arr in df_arr:
         res_arr.append(list(map(lambda x: np.log(1 + x), arr)))
 
-    columns = [
-        'total', 'under15', '15to64', 'over64', 'birth', 'death',
-        'transferee', 'out-migrant', 'daytime', 'elder', 'marriage', 'divorce']
-
-    df_res = pd.DataFrame(np.array(res_arr).T, columns=columns)
+    df_res = pd.DataFrame(np.array(res_arr).T)
+    df_res['name'] = df_name
+    df_res.columns = columns
     print(df_res)
-    df_res.to_csv('data/csv/input_log.csv', index=None, index_label=None)
+    df_res.to_csv('data/csv/input_log.csv', index=None, index_label=None, encoding='utf-8')
 
 
 if __name__ == '__main__':
