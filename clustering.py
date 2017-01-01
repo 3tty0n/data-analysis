1519fe7b9483165a9811934174045766b67a8c25
Yusuke Izawa <yuizalp@gmail.com>
2017-01-02 04:29:54 +0900

    fix clustering analysis and columns

--- clustering.py
+++ clustering.py
@@ -2,39 +2,16 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+from sklearn.cluster import KMeans
+from columns import columns
 
-"""
-0: 人口総数,
-1: "15歳未満人口",
-2: "15〜64歳人口",
-3: "65歳以上人口",
-4: 出生数,
-5: 死亡数,
-6: 転入者数,
-7: 転出者数,
-8: 昼間人口,
-9: "高齢単身世帯数",
-10: 婚姻件数,
-11: 離婚件数,
-12: 政府統計の総合窓口より2012年の人口状況(市町村名)
-"""
-
-df = pd.read_csv('data/csv/input.input_csv', header=None, skiprows=1, encoding='utf-8')
-del(df[0])
-del(df[4])
-del(df[5])
-del(df[6])
-del(df[11])
-numpy_df = np.array([df[1].tolist(),
-                     df[2].tolist(),
-                     df[3].tolist(),
-                     df[7].tolist(),
-                     df[8].tolist(),
-                     df[9].tolist(),
-                     df[10].tolist()], np.int32)
+df = pd.read_csv('data/csv/input.csv', encoding='utf-8')
+df_name = df['name']
+del df['name']
+lst = map(lambda index: df[columns[index]].tolist(), range(0, len(columns) - 1))
+numpy_df = np.array(list(lst), np.int32)
 numpy_df = numpy_df.T
 pred = KMeans(n_clusters=4).fit_predict(numpy_df)
 df['cluster_id'] = pred
--- columns.py
+++ columns.py
@@ -1,8 +1,3 @@
-columns_log = [
-    'total', 'under15', '15to64', 'over64', 'birth', 'death',
-    'transferee', 'out-migrant', 'daytime', 'elder', 'marriage', 'divorce'
-]
-
 columns = [
     'total', 'under15', '15to64', 'over64', 'birth', 'death',
     'transferee', 'out-migrant', 'daytime', 'elder', 'marriage', 'divorce', 'name'
