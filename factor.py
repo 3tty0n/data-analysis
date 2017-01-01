183b0725b01db5c2e5ff4a693a30f21215178e30
Yusuke Izawa <yuizalp@gmail.com>
2017-01-02 03:04:21 +0900

    add method comment

--- factor.py
+++ factor.py
@@ -8,12 +8,13 @@ from sklearn.decomposition import PCA
 from sklearn import svm
 
 
-def main():
-    fp = FontProperties(fname='/Library/Fonts/ヒラギノ丸ゴ ProN W4.ttc', size=8)
-    factor(fp=fp, is_save=False)
-
-
 def read_csv(is_log):
+    """
+    csvを読み込む
+    
+    :param is_log: log(1+x)とスケール変換されたファイルを読み込むフラグ
+    :return:
+    """
     if is_log is True:
         df = pd.read_csv('data/csv/input_log.csv', encoding='utf-8')
     else:
@@ -22,6 +23,12 @@ def read_csv(is_log):
 
 
 def calc_pca(df):
+    """
+    主成分分析を行う。
+
+    :param df:
+    :return:
+    """
     df = df.drop('name', axis=1)
 
     pca = PCA(n_components=2, whiten=False)
@@ -30,7 +37,36 @@ def calc_pca(df):
     return pca, pca_point
 
 
+def pca_components(is_log=True):
+    """
+    因子負荷量、寄与率、累積寄与率を計算しプロットする。
+
+    因子負荷量 : • 各変数の各主成分への影響⼒ → 各主成分の意味の推定
+    寄与率 : 各主成分の重要性
+    累積寄与率 : 主成分の寄与率を⾜し合わせたもの
+                選択した複数の主成分によって説明できるデータの割合を表す
+
+    :param is_log:
+    :return:
+    :see: http://i.cla.kobe-u.ac.jp/murao/class/2015-SeminarB3/05_Python_de_PCA.pdf
+    """
+    df = read_csv(is_log)
+    pca, pca_point = calc_pca(df)
+    components = pca.components_
+    ratio = np.cumsum(pca.explained_variance_ratio_)
+    print(components)
+    print(ratio)
+
+
 def factor(is_log=True, is_save=False, fp=None):
+    """
+    主成分分析を行った結果をプロットする。
+
+    :param is_log:
+    :param is_save:
+    :param fp:
+    :return:
+    """
     assert fp
 
     df = read_csv(is_log)
@@ -62,6 +98,12 @@ def factor(is_log=True, is_save=False, fp=None):
     elif is_save is True and is_log is False:
         plt.savefig('data/picture/factor.png')
     plt.show()
+
+
+def main():
+    fp = FontProperties(fname='/Library/Fonts/ヒラギノ丸ゴ ProN W4.ttc', size=8)
+    factor(fp=fp, is_save=False)
+    pca_components()
 
 
 if __name__ == '__main__':
